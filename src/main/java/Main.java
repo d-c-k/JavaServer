ee5b662c6cfdccc9634b5c681a06e6b94cdb9b24
Daniel Koefoed <dc.koefoed@gmail.com>
2021-11-23 10:16:09 +0100

    connect to db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
 package main.java;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.sun.net.httpserver.*;
+import org.bson.Document;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String connectionString = System.getProperty("mongodb.uri");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            System.out.println("Available databases:");
+            databases.forEach(db -> System.out.println(db.toJson()));
+        }
 
         int port = 3000;
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
