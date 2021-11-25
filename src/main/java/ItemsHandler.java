3e63c3f8527d5b9f3b587c97fdfb90152936d88d
Daniel Koefoed <dc.koefoed@gmail.com>
2021-11-25 14:51:41 +0100

    functional server with hideous code *trigger warning*

--- src/main/java/ItemsHandler.java
+++ src/main/java/ItemsHandler.java
@@ -1,81 +1,199 @@
 package main.java;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Updates;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import main.java.models.Item;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
+import java.util.Scanner;
+
+import static com.mongodb.client.model.Filters.eq;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class ItemsHandler implements HttpHandler {
 
     @Override
 
     public void handle(HttpExchange he) throws IOException {
-        URI uri = he.getRequestURI();
+        ConnectionString connectionString = new ConnectionString(System.getProperty("mongodb.uri"));
+        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
 
-        String fullPath = uri.getPath();
-        String subPath = fullPath.substring(0, fullPath.lastIndexOf("items") + 5);
-        String lastBit = getLastBitFromURI(fullPath);
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .codecRegistry(codecRegistry)
+                .build();
 
-        if (!Objects.equals(subPath, "/api/items")) {
-            he.sendResponseHeaders(400, 0);
-            he.close();
-        }
-        else if (isNumeric(lastBit)) {
-            int id = Integer.parseInt(lastBit);
+        try (MongoClient mongoClient = MongoClients.create(clientSettings)) {
+            MongoDatabase mainJavaDB = mongoClient.getDatabase("mainJava");
+            MongoCollection<Item> items = mainJavaDB.getCollection("items", Item.class);
 
-            if (Objects.equals(he.getRequestMethod(), "GET")) {
-                he.sendResponseHeaders(200, 0);
-                System.out.println("get item " + id);
-                he.close();
-            }
-            else if (Objects.equals(he.getRequestMethod(), "POST")) {
-                he.sendResponseHeaders(200, 0);
-                System.out.println("edit item " + id);
-                he.close();
-            }
-            else if (Objects.equals(he.getRequestMethod(), "DELETE")) {
-                he.sendResponseHeaders(200, 0);
-                System.out.println("delete item " + id);
-                he.close();
-            }
-            else {
+            URI uri = he.getRequestURI();
+
+            String fullPath = uri.getPath();
+            String subPath = fullPath.substring(0, fullPath.lastIndexOf("items") + 5);
+            String lastBit = getLastBitFromURI(fullPath);
+
+            InputStream is = he.getRequestBody();
+            OutputStream os = he.getResponseBody();
+
+            if (!Objects.equals(subPath, "/api/items")) {
                 he.sendResponseHeaders(400, 0);
                 he.close();
             }
-        }
-        else if (Objects.equals(lastBit, "items")) {
+            else if (Objects.equals(lastBit, "items")) {
+                    // Read all
+                    if (Objects.equals(he.getRequestMethod(), "GET")) {
+                        he.sendResponseHeaders(200, 0);
+                        ArrayList<Item> responseItems = new ArrayList<Item>();
+
+                        MongoCursor<Item> cursor = items.find().iterator();
+
+                        while (cursor.hasNext()) {
+                            responseItems.add(cursor.next());
+                        }
+
+                        GsonUtils gson = new GsonUtils();
+                        String json = gson.getGson().toJson(responseItems);
+
+                        os.write(json.getBytes());
+                        he.close();
+                    }
+                    // Create new
+                    else if (Objects.equals(he.getRequestMethod(), "POST")) {
+                        String input = parseInput(is);
+                        JsonObject json = new JsonParser().parse(input).getAsJsonObject();
+
+                        if (Objects.equals(json.get("title").getAsString(), "")) {
+                            he.sendResponseHeaders(400, 0);
+                            String errorMsg = "Todo-item requires a title";
+                            os.write(errorMsg.getBytes());
+                            he.close();
+                        }
+                        else if (Objects.equals(json.get("body").getAsString(), "")){
+                            he.sendResponseHeaders(400, 0);
+                            String errorMsg = "Todo-item requires a body";
+                            os.write(errorMsg.getBytes());
+                            he.close();
+                        }
+                        else {
+                            he.sendResponseHeaders(201, 0);
+
+                            Item newItem = new Item().setId(new ObjectId())
+                                    .setTitle(json.get("title").getAsString())
+                                    .setBody(json.get("body").getAsString())
+                                    .setCreatedAt(new Date())
+                                    .setUpdatedAt(new Date());
+                            items.insertOne(newItem);
+
+                            System.out.println("created new item");
+                            System.out.println(newItem);
+                            he.close();
+                        }
+                    } else {
+                        he.sendResponseHeaders(400, 0);
+                        System.out.println("bad request");
+                        he.close();
+                    }
+            }
+            else {
+                ObjectId id = new ObjectId(lastBit);
+
+                // Get single item
                 if (Objects.equals(he.getRequestMethod(), "GET")) {
-                    he.sendResponseHeaders(200, 0);
-                    System.out.println("get all items");
-                    he.close();
-                } else if (Objects.equals(he.getRequestMethod(), "POST")) {
-                    he.sendResponseHeaders(201, 0);
-                    System.out.println("created new item");
-                    he.close();
-                } else {
-                    he.sendResponseHeaders(400, 0);
-                    System.out.println("bad request");
+                    Item item = items.find(eq("_id", id)).first();
+
+                    if (item == null) {
+                        he.sendResponseHeaders(404, 0);
+                    }
+                    else {
+                        he.sendResponseHeaders(200, 0);
+                        GsonUtils gson = new GsonUtils();
+                        String json = gson.getGson().toJson(item);
+                        os.write(json.getBytes());
+                    }
                     he.close();
                 }
-        }
-        else {
-            he.sendResponseHeaders(404, 0);
-            he.close();
-        }
-    }
+                // Edit single item
+                else if (Objects.equals(he.getRequestMethod(), "POST")) {
+                    String input = parseInput(is);
+                    JsonObject json = new JsonParser().parse(input).getAsJsonObject();
 
-    private static boolean isNumeric(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+                    if (Objects.equals(json.get("title").getAsString(), "")) {
+                        he.sendResponseHeaders(400, 0);
+                        String errorMsg = "Todo-item requires a title";
+                        os.write(errorMsg.getBytes());
+                        he.close();
+                    }
+                    else if (Objects.equals(json.get("body").getAsString(), "")){
+                        he.sendResponseHeaders(400, 0);
+                        String errorMsg = "Todo-item requires a body";
+                        os.write(errorMsg.getBytes());
+                        he.close();
+                    }
+                    else {
+                        he.sendResponseHeaders(202, 0);
+
+                        Bson query = eq("_id", id);
+
+                        Bson updates = Updates.combine(
+                                Updates.set("title", json.get("title").getAsString()),
+                                Updates.set("body", json.get("body").getAsString()),
+                                Updates.set("updatedAt", new Date()));
+
+                        items.updateOne(query, updates);
+                        he.close();
+                    }
+                }
+                // Delete single item
+                else if (Objects.equals(he.getRequestMethod(), "DELETE")) {
+                    Item item = items.find(eq("_id", id)).first();
+
+                    if (item == null) {
+                        he.sendResponseHeaders(404, 0);
+                    }
+                    else {
+                        he.sendResponseHeaders(200, 0);
+                        Bson query = eq("_id", id);
+                        items.deleteOne(query);
+                    }
+                    he.close();
+                }
+                else {
+                    he.sendResponseHeaders(400, 0);
+                    he.close();
+                }
+            }
+        } catch (Exception e) {
+            he.sendResponseHeaders(500, 0);
+            he.close();
         }
     }
 
     private static String getLastBitFromURI(String uri) {
         return uri.replaceFirst(".*/([^/?]+).*", "$1");
+    }
+
+    private static String parseInput(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        String input = s.hasNext() ? s.next() : "";
+        return input;
     }
 }
--- src/main/java/models/Item.java
+++ src/main/java/models/Item.java
@@ -1,4 +1,86 @@
 package main.java.models;
 
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+import java.util.Objects;
+
 public class Item {
+
+    @BsonProperty(value = "_id")
+    private ObjectId _id;
+    private String title;
+    private String body;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public ObjectId getId() {
+        return _id;
+    }
+
+    public Item setId(ObjectId id) {
+        this._id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Item setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Item setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Item setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Item setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + _id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(_id, item._id) && Objects.equals(title, item.title) && Objects.equals(body, item.body) && Objects.equals(createdAt, item.createdAt) && Objects.equals(updatedAt, item.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, title, body, createdAt, updatedAt);
+    }
 }
